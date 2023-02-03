2480a2041097e2707c71af8092986624f7931ca6
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-02-03 18:39:00 +0200

    task 1

---
+++ js/task-01.js
@@ -0,0 +1,30 @@
+
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+
+// Скрипт для виведення в консоль загальної к-сті категорій
+
+const allElementsList = document.querySelectorAll(`.item`);
+console.log('Number of categories :', allElementsList.length);
+
+// Скрипт для перебору масиву та  виведення в консоль <h2> і його <li>
+
+allElementsList.forEach((item) => {
+    console.log(`Category:`, item.firstElementChild.textContent);
+    console.log(`Elements:`, item.lastElementChild.childElementCount);
+})
+
+
+ 
