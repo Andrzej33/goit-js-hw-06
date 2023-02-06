5900eef092b1b01ba6bd180a35ab9027981c846a
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-02-06 12:27:28 +0200

    task-10 start

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,65 @@
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+
+// Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+
+const refs = {
+  input:
+    document.querySelector('#controls > input'),
+ createBtn :
+  document.querySelector('button[data-create]'),
+ destroyBtn :
+  document.querySelector('button[data-destroy]'),
+ collection :
+  document.querySelector('#controls'),
+}
+
+
+
+refs.input.addEventListener('input', (event) => {
+  const amountBoxes = event.currentTarget.value
+ });
+
+
+
+
+refs.createBtn.addEventListener('click', () => { });
+refs.destroyBtn.addEventListener('click', () => { });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
