4865e202d107109afd997a5520ca41dfac09c18d
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-02-07 15:38:48 +0200

    corection

--- js/task-06.js
+++ js/task-06.js
@@ -8,12 +8,13 @@ input.addEventListener("blur", onInputBlur);
 
 // Виносимо колбек в окрему функцію
 
-function onInputBlur(event) {
-  if (event.currentTarget.value.length == input.getAttribute("data-length")) {
+function onInputBlur() {
+  if (input.value.length === +input.getAttribute("data-length")) {
     input.classList.add("valid");
     input.classList.remove("invalid");
   } else {
     input.classList.add("invalid");
     input.classList.remove("valid");
   }
+ 
 }
